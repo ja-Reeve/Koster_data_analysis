f71cd6ebee7d8b1dbaebe548869ab5208e641de3
ja-Reeve <82411887+ja-Reeve@users.noreply.github.com>
2026-01-06 15:09:59 +0100

    Update variance_explained.R
    Overhaul of script to use Bayes R2 scores to calculate variance explained for brms models. 
    This has the added benefit of being able to also estimate standard deviations around variance estimates.

--- Bayesian_regression/variance_explained.R
+++ Bayesian_regression/variance_explained.R
@@ -1,215 +1,166 @@
 ################################### Variance explained by brms models #################################
 ### Determining the variance explained by each tested model. For models with a phenotype as a response, 
-### variance is calculated from the residuals of the brms model. For the models with inversion 
-### arrangements as response variables, variance is instead estimated from deviance.
+### variance is calculated from the residuals of the brms model. Variance explained is estimated as by
+### calclating Bayes R^2 (Gelman et al., 2018) for a series of submodels.
 
-### James Reeve - University of Gothenburg
-### 2024-1-22
+### James Reeve - DTU Aqua
+### 2025-11-20
 
 ### Preparation
-rm(list = ls())
-dev.off()
+rm(list=ls())
+options(stringsAsFactors = FALSE)
+#options(repos=structure(c(CRAN="https://ftp.acc.umu.se/mirror/CRAN/")))
+#options(mc.cores = parallel::detectCores())
 
 ### Packages
+### Only run the first line once. It is a waste of time to reinstall pacakges
+#install.packages("brms", dependencies = TRUE, INSTALL_opts = "--no-lock")
+#install.packages('RcppEigen')
+library(dplyr)
+library(rstan)
 library(brms)
-library(ggplot2)
 
-### Filepath
-PATH <- "/Volumes/PhD_backup_JR/brms_fits/"
+# R-stan options 
+rstan_options(auto_write = TRUE)
+#rstan_options(threads_per_chain = 2)
 
-#### Parameters ####
-Vars <- c("PC1.pheno", "PC2.pheno", "PC3.pheno")
-Habs <- c("rock", "boulder", "mud-sand", "hab.only")
-Invs <- c("LGC1.1", "LGC1.2", "LGC2.1", "LGC4.1", "LGC6.1.2", "LGC6.1.2b", "LGC7.1", "LGC7.2", "LGC9.1", 
-          "LGC10.1", "LGC10.2", "LGC11.1", "LGC12.1", "LGC12.2", "LGC12.3", "LGC12.4", "LGC14.1", "LGC14.3",
-          "LGC17.1")
+### Filepaths
+PATH <- "/path/to/brms/model/fits/"
+
+### Parameters
+#Params <- commandArgs(trailingOnly = TRUE)
+Analysis <- "Pheno_InvEnv"
+# Possible values = "Pheno_Inv", "Pheno_Env", "Inv_Env", and "Pheno_InvEnv"
+
+Var <- "PC1.pheno"
+# Possible values = "PC1.pheno", "PC2.pheno", and "shellLength"
+
+Hab <- "mud-sand"
+# Possible values = "rock", "boulder", "mud-sand", "hab.only", and "Inv"
 
 
-#### Function to calculate varaince explained: ####
 
-Var.Exp <- function(variable, habitat){
-  if(Analysis == "Pheno_InvEnv"){
-    # Final model fits (Pheno~Inv+Env models)
-    load(paste0(PATH, Analysis, "/brms_fit.", variable, "_InvEnv_", habitat)) # Full model
-    load(paste0(PATH, Analysis, "/brms_fit.", variable, "_InvEnv_", habitat, "_null")) # Null model
-    
+#### Function to calculate variance explained: ####
+
+Var.Exp <- function(variable, habitat) {
+  
+  ## A: Get R^2 from submodels
+  # Variance expalined by Pheno~Inv model
+  load(paste0(PATH, "Pheno_Inv/brms_fit.",
+              ifelse(variable != "shellLength", substr(variable, 1, 3), variable),
+              "_Inv_v2"))
+  R2.Pheno_Inv <- bayes_R2(mod, summary = FALSE)
+  
+  # Variance explained by Pheno~Env model
+  load(paste0(PATH, "Pheno_Env/brms_fit.", variable, "_", habitat, "_v2"))
+  R2.Pheno_Env <- bayes_R2(mod, summary = FALSE)
+  
+  # Variance explained by full model (Pheno~InvEnv)
+  # Note: order of operations is important. This should be run after the last two models.
+  load(paste0(PATH, "Pheno_InvEnv/brms_fit.", variable, "_InvEnv_", habitat, "_v2"))
+  R2.full <- bayes_R2(mod, summary = FALSE)
+  
+  
+  ## B: Run four new submodels
+  # Access components from 'mod'
+  dat <- mod$data
+  GCov <- mod$data2 # genetic covariance matrix
+  fml <- mod$formula
+  
+  # 1) Variance explained without genetic covariance matrix
+  mod_noGBG <- brm(update.formula(fml, .~.-(1|gr(snail_ID, cov = GD))),
+                   data = dat, data2 = GCov,
+                   iter = 4000, chains = 4, cores = 2L, threads = threading(2))
+  mod_noGBG <- add_criterion(mod_noGBG, criterion = "loo")
+  R2.noGBG <- bayes_R2(mod_noGBG, summary = FALSE)
+  rm(mod_noGBG) # Removing to save memory
+  
+  # 2) Variance explained without co-factors
+  mod_noCF <- brm(update.formula(fml, .~.-sex-adult-(1|Date)),
+                  data = dat, data2 = GCov,
+                  iter = 4000, chains = 4, cores = 2L, threads = threading(2))
+  mod_noCF <- add_criterion(mod_noCF, criterion = "loo")
+  R2.noCF <- bayes_R2(mod_noCF, summary = FALSE)
+  rm(mod_noCF)
+  
+  # 3) Total variance explained by environment (variance without confounds)
+  mod_onlyEnv <- brm(update.formula(fml, .~.-sex-adult-(1|gr(snail_ID, cov = GD))-(1|Date)
+                                    -LGC1.1-LGC1.2-LGC2.1-LGC4.1-LGC6.1.2-LGC6.1.2b-LGC7.1-LGC7.2
+                                    -LGC9.1-LGC10.1-LGC10.2-LGC11.1-LGC12.1-LGC12.2-LGC12.3-LGC12.4
+                                    -LGC14.1-LGC14.3-LGC17.1),
+                     data = dat, data2 = GCov,
+                     iter = 4000, chains = 4, cores = 2L, threads = threading(2))
+  mod_onlyEnv <- add_criterion(mod_onlyEnv, criterion = "loo")
+  R2.onlyEnv <- bayes_R2(mod_onlyEnv, summary = FALSE)
+  rm(mod_onlyEnv)
+  
+  # 4) Total variance explained by inversions (variance without confounds)
+  # There's a slightly different formula for hab.only models
+  if(habitat == "hab.only"){
+    mod_onlyInv <- brm(update.formula(fml, .~.-sex-adult-(1|gr(snail_ID, cov = GD))-(1|Date)
+                                      -Habitat),
+                       data = dat, data2 = GCov,
+                       iter = 4000, chains = 4, cores = 2L, threads = threading(2))
   } else {
-    # First round of model fits
-    load(paste0(PATH, Analysis, "/brms_fit.", variable, "_", habitat)) # Full model
-    load(paste0(PATH, Analysis, "/brms_fit.", variable, "_", habitat, "_null")) # Null model
-    
+    mod_onlyInv <- brm(update.formula(fml, .~.-sex-adult-(1|gr(snail_ID, cov = GD))-(1|Date)
+                                      -PC1.env-PC2.env-PC3.env),
+                       data = dat, data2 = GCov,
+                       iter = 4000, chains = 4, cores = 2L, threads = threading(2))
   }
+  mod_onlyInv <- add_criterion(mod_onlyInv, criterion = "loo")
+  R2.onlyInv <- bayes_R2(mod_onlyInv, summary = FALSE)
+  rm(mod_onlyInv)
   
-  ### LOOIC scores
-  looic.mod <- mod$criteria$loo$estimates[3,1]
-  looic.null <- mod_null$criteria$loo$estimates[3,1]
   
-  ### Delta-LOOIC
-  if(looic.mod > looic.null){
-    dlooic <- abs(loo_compare(mod, mod_null)[2,1])
-  } else {
-    dlooic <- loo_compare(mod, mod_null)[2,1]
-  }
-  dlooic.se <- loo_compare(mod, mod_null)[2,2]
+  ## C: Calculate remaining variance components
+  # Variance explained by inversions - with confounds
+  R2.Inv <- R2.full - R2.Pheno_Env
   
-  if(Analysis == "Inv_Env"){
-    
-    ### Get fitted values
-    fits <- predict(mod)[,1]
-    # Transform to inversion frequencies
-    fits <- fits/2
-    
-    ### Fit glm comparing observations to fitted values to calculate deviance
-    devi <- glm(cbind(mod$data$Inv, 2-mod$data$Inv) ~ fits,
-                family = binomial(link = "logit"))
-    
-    ### Calculate deviance explained by model terms
-    dev_mod <- devi$deviance
-    dev_null <- devi$null.deviance
-    dExp <- ( dev_null - dev_mod ) / dev_null
-    
-    ### Calculate dispersion
-    disp <- dev_null / devi$df.residual
-    
-    ### Create data frame
-    res <- data.frame("Analysis" = Analysis,
-                      "Variable" = variable,
-                      "Habitat" = habitat,
-                      dev_mod, dev_null, 
-                      "Dexp" = dExp,
-                      "disersion" = disp,
-                      looic.mod, looic.null, dlooic, dlooic.se)
-    
-    } else {
-    
-    ### Get residual variance
-    # Tested values
-    res_mod <- VarCorr(mod)$residual$sd[1]^2
-    # Model with just sex, maturity and genetic covariance
-    res_null <- VarCorr(mod_null)$residual$sd[1]^2
-    
-    ### Get total variance
-    Tvar <- var(mod$data$Pheno)
-    
-    ### % Variance explained
-    Vexp <- ( (res_null - res_mod) / Tvar ) * 100
-    
-    ### Create data frame
-    res <- data.frame("Analysis" = Analysis,
-                      "Variable" = variable,
-                      "Habitat" = habitat,
-                      "Vmod" = res_mod,
-                      "Vnull" = res_null,
-                      "Vtotal" = Tvar, 
-                      Vexp, looic.mod, looic.null, dlooic, dlooic.se)
-  }
+  # Variance explained by environment - with confounds
+  R2.Env <- sample(R2.full, length(R2.Pheno_Inv), replace = F) - R2.Pheno_Inv
+  
+  # Variance explained by co-factors (sex, maturity and Date)
+  R2.CF <- R2.full - R2.noCF
+  
+  # Variance explained by factors without genetic background
+  R2.GBG <- R2.full - R2.noGBG
+  
+  # Confounded variance with inversions
+  R2.confInv <- R2.onlyInv - R2.Inv
+  
+  # Confounded variance with environment
+  R2.confEnv <- sample(R2.onlyEnv, length(R2.Env), replace = F) - R2.Env
+  
+  
+  ## D: Write data frame
+  res <- data.frame("Analysis" = Analysis,
+                    "Variable" = variable,
+                    "Habitat" = habitat,
+                    "R2full" = mean(R2.full),
+                    "R2full.SD" = sd(R2.full),
+                    "R2onlyInv" = mean(R2.onlyInv),
+                    "R2onlyInv.SD" = sd(R2.onlyInv),
+                    "R2onlyEnv" = mean(R2.onlyEnv),
+                    "R2onlyEnv.SD" = sd(R2.onlyEnv),
+                    "R2inv" = mean(R2.Inv),
+                    "R2inv.SD" = sd(R2.Inv),
+                    "R2env" = mean(R2.Env),
+                    "R2env.SD" = sd(R2.Env),
+                    "R2cf" = mean(R2.CF),
+                    "R2cf.SD" = sd(R2.CF),
+                    "R2gbg" = mean(R2.GBG),
+                    "R2gbg.SD" = sd(R2.GBG),
+                    "R2confInv" = mean(R2.confInv),
+                    "R2confInv.SD" = sd(R2.confInv),
+                    "R2confEnv" = mean(R2.confEnv),
+                    "R2confEnv.SD" = sd(R2.confEnv))
   return(res)
+  
 }
 
 
-### Pheno~Env
-Analysis <- "Pheno_Env"
-PE.Vexps <- lapply(Habs, function(h){
-  tmp <- lapply(Vars, Var.Exp, habitat = h)
-  tmp <- do.call(rbind.data.frame, tmp)
-  return(tmp)
-})
-PE.Vexps <- do.call(rbind.data.frame, PE.Vexps)
+### Run function
+Vexp <- Var.Exp(variable = Var, habitat = Hab)
 
-ggplot(PE.Vexps)+
-  geom_point(aes(x = factor(Habitat, levels = Habs), y = Variable, 
-                 size = Vexp, colour = dlooic < -4 | dlooic < -2*dlooic.se), show.legend = FALSE)+
-  scale_size_binned(breaks = seq(0, 30, 5), limits = c(0, 30))+
-  scale_colour_manual(values = c("grey80", "#008080ff"))+
-  labs(title = "Phenotype ~ Environment")+
-  theme_void()+
-  theme(axis.text = element_text(size = 14),
-        panel.grid = element_line(linewidth = 0.4, colour = "grey95"))
-# 400 X 120
-
-
-### Pheno~Inv
-Analysis <- "Pheno_Inv"
-PI.Vexp <- lapply(substr(Vars, 1, 3), Var.Exp, habitat = "Inv")
-PI.Vexps <- do.call(rbind.data.frame, PI.Vexp)
-
-ggplot(PI.Vexps)+
-  geom_point(aes(x = Habitat, y = Variable, 
-                 size = Vexp, colour = dlooic < -4 | dlooic < -2*dlooic.se), show.legend = FALSE)+
-  scale_size_binned(breaks = seq(0, 30, 5), limits = c(0, 30))+
-  scale_colour_manual(values = c("grey80", "#008080ff"))+
-  labs(title = "Phenotype ~ Inversion")+
-  theme_void()+
-  theme(axis.text = element_text(size = 14),
-        panel.grid = element_line(linewidth = 0.4, colour = "grey95"))
-# 120 x 120
-
-### Pheno~Inv+Env
-Analysis <- "Pheno_InvEnv"
-PIE.Vexps <- lapply(Habs[-3], function(h){
-  tmp <- lapply(Vars[-2], Var.Exp, habitat = h)
-  tmp <- do.call(rbind.data.frame, tmp)
-  return(tmp)
-})
-PIE.Vexps <- do.call(rbind.data.frame, PIE.Vexps)
-
-ggplot(PIE.Vexps)+
-  geom_point(aes(x = factor(Habitat, levels = Habs[-3]), y = Variable, 
-                 size = Vexp, colour = dlooic < -4 | dlooic < -2*dlooic.se), show.legend = FALSE)+
-  scale_size_binned(breaks = seq(0, 30, 5), limits = c(0, 30))+
-  scale_colour_manual(values = c("grey80", "#008080ff"))+
-  labs(title = "Phenotype ~ Inversion + Environment")+
-  theme_void()+
-  theme(axis.text = element_text(size = 14),
-        panel.grid = element_line(linewidth = 0.4, colour = "grey95"))
-# 300 x 100
-
-
-### Inv~Env
-Analysis <- "Inv_Env"
-IE.Vexps <- lapply(Habs, function(h){
-  tmp <- lapply(Invs, Var.Exp, habitat = h)
-  tmp <- do.call(rbind.data.frame, tmp)
-  return(tmp)
-})
-IE.Vexps <- do.call(rbind.data.frame, IE.Vexps)
-
-ggplot(IE.Vexps)+
-  geom_point(aes(x = factor(Habitat, levels = Habs), y = factor(Variable, levels = Invs), 
-                 size = Dexp, colour = dlooic < -4 | dlooic < -2*dlooic.se), show.legend = FALSE)+
-  scale_size_binned(breaks = seq(0, 1, 0.1), limits = c(0, 1))+
-  scale_colour_manual(values = c("grey80", "#008080ff"))+
-  labs(title = "Inversion ~ Environment")+
-  theme_void()+
-  theme(axis.text = element_text(size = 14),
-        panel.grid = element_line(linewidth = 0.4, colour = "grey95"))
-# 400 X 500 (W x H)
-
-
-
-#### Variance decomposition: finding variance attributed to inversions and variance from plasticity (or confounds) ####
-### Variance explained by inversions = PIE.Vexps - PE.Vexps
-
-# Merge PIE.Vexps and PE.Vexps
-tmp <- left_join(PIE.Vexps, PE.Vexps, by = c("Variable", "Habitat"), suffix = c(".PIE", ".PE")) %>%
-  #filter(Habitat != "hab.only") %>% 
-  summarise(Variable, Habitat,
-            "Vexp.Inv" = Vexp.PIE - Vexp.PE,
-            "Vexp.Env" = Vexp.PE,
-            "Vexp.cf" = ((Vtotal.PIE - Vnull.PIE) / Vtotal.PIE) * 100) %>%
-  mutate("Vunexp" = 100 - Vexp.Inv - Vexp.Env - Vexp.cf) %>%
-  pivot_longer(cols = c(Vexp.Inv, Vexp.Env, Vexp.cf, Vunexp), names_to = "type", values_to = "Vexp")
-
-# Manual adjustments of negative Vexp for PC3.pheno rock
-tmp[tmp$Variable == "PC3.pheno" & tmp$Habitat == "rock" & tmp$type == "Vexp.Env", "Vexp"] <- 0
-tmp[tmp$Variable == "PC3.pheno" & tmp$Habitat == "rock" & tmp$type == "Vunexp", "Vexp"] <- 76.7
-
-### Pie charts
-ggplot(tmp, aes(x = "", y = Vexp, fill = type))+
-  geom_bar(stat = "identity", position = "fill", col = "grey50")+
-  coord_polar("y", start = 0)+
-  scale_fill_manual(values = c("grey70", "#eff7f7ff", "#008080ff", "white"))+
-  facet_grid(rows = vars(factor(Variable, levels = c("PC3.pheno", "PC1.pheno"))), 
-             cols = vars(factor(Habitat, levels = c("rock", "boulder", "hab.only"))))+
-  theme_void()
-# 800 x 400
+# Save results
+write.csv(Vexp, paste0(PATH, "variance_explained/", Analysis, "_", Var, "_", Hab, "_var_exp.csv"), row.names = FALSE, quote = FALSE)
